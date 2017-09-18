a731975b0018a8b077cf5190b022af0c614fdbe3
HozStone <stone821122@gmail.com>
2017-09-18 20:17:46 +0800

    add a function to record data

--- gyro.js
+++ gyro.js
@@ -1,7 +1,10 @@
 var app = require('express')();
+var fs= require('fs');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/gyro.html');
@@ -12,9 +15,14 @@ io.on('connection', function(socket){
 	console.log("Connecting.....");
     
     socket.on('device', function(obj){
-     console.log("accelerationX:"+ obj.accelerationX+"  accelerationY:"+obj.accelerationY+"  accelerationZ:"+obj.accelerationZ);
-     console.log("alpha:"+ obj.alpha+"  beta:"+obj.beta+"  gamma:"+obj.gamma);
-     console.log("==================================================================================");
+      
+     var data=[obj.accelerationX,obj.accelerationY,obj.accelerationZ,obj.alpha,obj.beta,obj.gamma];
+
+     var file = Convertjson(data);
+     var jsonText = JSON.stringify(file[1]," "," ");//only first data
+      
+     console.log(jsonText);
+     fs.appendFile("./result/_result_.json",jsonText);
 
   });
 
@@ -24,6 +32,28 @@ io.on('connection', function(socket){
 });
 
  
+
+
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+//-----------------------sub function------------------------------------------------------
+
+ 
+function Convertjson(Array){
+
+  var file =[];
+  for(var k=0;k<Array.length;k++){
+    file[k]={
+        accelerationX:Array[0],
+        accelerationY:Array[1],
+        accelerationZ:Array[2],
+        alpha:Array[3],
+        beta:Array[4],
+        gamma:Array[5],
+    }
+  }
+   return file ;
+}
+
