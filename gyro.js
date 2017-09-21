b7694d067ebccaeae1126ae2a62eef9ef7b7449b
HozStone <stone821122@gmail.com>
2017-09-21 10:30:56 +0800

    Add files via upload
    fix json type

--- gyro.js
+++ gyro.js
@@ -16,18 +16,29 @@ io.on('connection', function(socket){
     
     socket.on('device', function(obj){
       
-     var data=[obj.accelerationX,obj.accelerationY,obj.accelerationZ,obj.alpha,obj.beta,obj.gamma];
+   // var time =[date,hour,min,sec,millsec];
+     
+     var time=obj.year+"年"+obj.month+"月"+obj.date+"日"+obj.hours+"點"+obj.minutes+"分"+obj.seconds+"秒"+obj.msec+"毫秒";
+     
+
+
+     var data=[obj.accelerationX,obj.accelerationY,obj.accelerationZ,obj.alpha,obj.beta,obj.gamma,time];
+
+
 
      var file = Convertjson(data);
-     var jsonText = JSON.stringify(file[1]," "," ");//only first data
-      
+    
+     var jsonText = JSON.stringify(file,","," ");//only first data
+     /* 
      console.log(jsonText);
-     fs.appendFile("./result/_result_.json",jsonText);
+  */  
+     fs.appendFile("./result/_result_"+obj.year+"y"+obj.month+"m"+obj.date+"d"+obj.hours+"h"+obj.minutes+"m"+".json",jsonText);
+     console.log(jsonText);
 
   });
 
     socket.on('disconnect', function(){
-    console.log('Disconnected');
+     console.log('Disconnected');
   });
 });
 
@@ -52,6 +63,7 @@ function Convertjson(Array){
         alpha:Array[3],
         beta:Array[4],
         gamma:Array[5],
+        time:Array[6]
     }
   }
    return file ;
