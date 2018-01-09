2a058202bb4ee349159285f02285f3788a18c972
HozStone <stone821122@gmail.com>
2018-01-09 12:22:04 +0800

    Add files via upload

--- gyro.js
+++ gyro.js
@@ -6,7 +6,8 @@ var port = process.env.PORT || 3000;
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
  
-var url = 'mongodb://127.0.0.1:27017/test';
+const url = 'mongodb://localhost:27017';
+const dbName = 'test';
 
 
 app.get('/', function(req, res){
@@ -19,52 +20,49 @@ io.on('connection', function(socket){
     
 
 // use socket.io to get data from device
-    socket.on('device', function(obj){
+  socket.on('device', function(obj){
 
   //build data
      var time=(obj.minutes*100000)+ (obj.seconds*1000)+ obj.msec ;
      var data=[obj.accelerationX,obj.accelerationY,obj.accelerationZ,obj.alpha,obj.beta,obj.gamma,obj.year,obj.month,obj.date,obj.hours,time];
-     
 
- 
-  //connect Mongodb    
-    MongoClient.connect(url, function (err,db) { 
-      assert.equal(null, err); 
-    console.log("Connected correctly to server"); 
-    //create a collection
-    var gait = db.collection('gaitdata'); //data、data_2
-    
-      var file = {
+     var file = {
            accelerationX:data[0],
            accelerationY:data[1],
            accelerationZ:data[2],
 
-          alpha:data[3],
-          beta:data[4],
-          gamma:data[5],
+           alpha:data[3],
+           beta:data[4],
+           gamma:data[5],
 
 
-          year:data[6],
-          month:data[7],
-          date:data[8],
-          hours:data[9],
-          sec:data[10]
-          
+           year:data[6],
+           month:data[7],
+           date:data[8],
+           hours:data[9],
+           sec:data[10] 
       };
 
-   //insert file
-    gait.insert(file, function (err, doc) { 
-        if (err) { 
-            db.close(); 
-            console.log("Error!!!");
-            return console.error(err); 
-        } 
-        console.log("資料已新增 : "+ file); 
-    }); 
- 
+    MongoClient.connect(url, function(err, client) {
+      assert.equal(null, err);
+      console.log("Connected successfully to server");
+
+      const db = client.db(dbName);
+
+      insertDocuments(db,function() {
+        client.close();
+      });
     });
 
-    /// fs.appendFile("./result/_result_"+obj.year+"y"+obj.month +1 +"m"+obj.date+"d"+obj.hours+"h"+obj.minutes+"m"+".json",jsonText);
+    const insertDocuments = function(db,callback){
+      const collection = db.collection('gait');
+      
+      collection.insert(file,function(err,result){
+        assert.equal(err,null);
+        console.log("資料已新增");
+        callback(result);
+      });
+    }
 
   });
   //disconnect
@@ -77,6 +75,7 @@ io.on('connection', function(socket){
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
 
 
 
